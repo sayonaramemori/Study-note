190736539868c9361ec6d7d1f14e5e2cafb34ec7
sayonara <1342733420@qq.com>
2021-08-23 19:46:42 -0700

    update

--- code/interstr.cpp
+++ code/interstr.cpp
@@ -16,7 +16,8 @@ public:
     }
 private:
 	bool check(const string&s1,const string&s2,const string&s3);
-	int moreMat(bool isOne,int&,int&,const string&,const string&);
+	int index;
+	int moreMat(int,int,const string&,const string&);
 	bool flag = false;
 	int p1;
 	size_t sz1;
@@ -25,41 +26,61 @@ private:
 	int p3;
 	size_t sz3;
 };
-void Solution::moreMat(bool isOne,int &m,int &s,const string& ms,const string &s3){
-	++m;
-	++s;
-	if(isOne){
-		if(m!=sz1&&s!=sz3)
-			if(ms[m]==s3[s])moreMat(true,m,s,ms,s3);
-	}else{
-		if(m!=sz2&&s!=sz3)
-			if(ms[m]==s3[s])moreMat(false,m,s,ms,s3);
+int Solution::moreMat(int p1,int p2,const string &s1,const string &s2){
+	index = 0;
+	while(p1<s1.size()&&s1[p1]==s2[p2]){
+		++index;
+		++p1;
+		++p2;
 	}
-	return;
+	return index;
 }
 bool Solution::check(const string&s1,const string&s2,const string&s3){
+	int res;
+	int tmp;
 	while(1){
-		if(p1==sz1||p2==sz2||p3==sz3){
-			if(p1==sz1&&p2==sz2&&p3==sz3&&(p1+p2)==p3){
+		if(p1==sz1||p2==sz2){
+			if((p1+p2)==sz3){
 				flag=true;
 				return true;
 			}
 			else{
 				if(p1==sz1){
-					moreMat(true,p2,p3,s2,s3);
-					continue;
+					res = moreMat(p2,p3,s2,s3);
+					p2 += res;
+					p3 += res;
 				}else{
-					moreMat(false,p1,p3,s2,s3);
-					continue;
+					res = moreMat(p1,p3,s2,s3);
+					p1 += res;
+					p3 += res;
 				}
+				flag = true;
+				if((p1+p2)==p3)return true;
+				else return false;
 			}
 		}
-		if(s1[p1]==s3[p3]){
-			moreMat(true,p1,p3,s1,s3);
-			continue;
-		}
-		if(s2[p2]==s3[p3]){
-			moreMat(false,p2,p3,s2,s3);
+		if(s2[p2]==s3[p3]||s1[p1]==s3[p3]){
+			if(s2[p2]==s3[p3]&&s3[p3]==s1[p1]){
+				res = moreMat(p1,p3,s1,s3);
+				tmp = moreMat(p2,p3,s2,s3);
+				if(res>tmp){
+					p1+=res;
+					p3+=res;
+				}else{
+					p2+=tmp;
+					p2+=tmp;
+				}
+			}else{
+				if(s2[p2]==s3[p3]){
+					res = moreMat(p2,p3,s2,s3);
+					p2 += res;
+					p3 += res;
+				}else{
+					res = moreMat(p1,p3,s1,s3);
+					p1 += res;
+					p3 += res;
+				}
+			}
 			continue;
 		}
 		return false;
